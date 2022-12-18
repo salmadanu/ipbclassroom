65d56ec83a7cf7d6d221bb54a511e10ec335543e
Lutfiahn <93109340+piyaww@users.noreply.github.com>
2022-12-19 00:23:34 +0700

    Update peminjaman.php

--- peminjaman.php
+++ peminjaman.php
@@ -2,28 +2,33 @@
     include('config.php');
     if(isset($_POST['pinjamruangan'])){
 
+	// ambil data dari formulir
     $nama_acara = $_POST['nama_acara'];
     $deskripsi_acara = $_POST['deskripsi_acara'];
     $ruangan_fakultas = $_POST['ruangan_fakultas'];
     $kode_ruangan = $_POST['kode_ruangan'];
     $waktu_mulai = $_POST['waktu_mulai'];
+        $temp_time = strtotime($waktu_mulai);
+        $waktu_mulai = date('Y-m-d H:i:s', $temp_time);
     $waktu_selesai = $_POST['waktu_selesai'];
+        $temp_time = strtotime($waktu_selesai);
+        $waktu_selesai = date('Y-m-d H:i:s', $temp_time);
     $notelfon_pj = $_POST['notelfon_pj'];
-    $nama_ruangan_query = pg_query("SELECT ruangan_nama FROM ruangan WHERE kode_ruangan = '$kode_ruangan'");
-    $nama_ruangan = pg_fetch_array($nama_ruangan_query);
-    var_dump($nama_ruangan);
-    var_dump($kode_ruangan);
 
-   $query = pg_query("INSERT INTO peminjaman (nama_acara, deskripsi_acara, notelfon_pj, waktu_mulai, waktu_selesai) 
-            VALUES ($nama_acara, $deskripsi_acara, $notelfon_pj, '$waktu_mulai', '$waktu_selesai')");
 
+	// buat query
+   $query = pg_query("INSERT INTO peminjaman (kode_ruangan_peminjaman, nama_acara, waktu_mulai, waktu_selesai, notelfon_pj, disetujui) VALUES ('$kode_ruangan', '$nama_acara', '$waktu_mulai', '$waktu_selesai', $notelfon_pj, 'f')");
+
+	// apakah query simpan berhasil?
 	if( $query==TRUE ) {
-		kalau berhasil alihkan ke halaman index.php 
+		// kalau berhasil alihkan ke halaman index.php 
 		header('Location: history_user.php?status=berhasil');
 	} else {
-		kalau gagal kembalikan ke halaman form
-		header('Location: reservasi_user.php');
+		// kalau gagal kembalikan ke halaman form
+		header('Location: reservasi_user.php?');
 	}
+
+
     } else {
         die("Akses dilarang...");
     }
